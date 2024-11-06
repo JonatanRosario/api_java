2018a6e1a6a46c99ba6f0bcf5555a823940593f2
Jonatan Rosario <jonatan_rosario4@hotmail.com>
2024-11-05 21:04:21 -0300

    add url

--- api/src/main/java/br/com/jonatanrosario/api/config/SwaggerConfig.java
+++ api/src/main/java/br/com/jonatanrosario/api/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("Gerenciamento de Produtos API")
+                .title("Gerenciamento de Produtos API.")
                 .version("1.0")
                 .description("API para realizar operações de CRUD (Create, Read, Update, Delete) em um sistema de gerenciamento de produtos. \n" +
                         "Permite o cadastro, consulta, atualização e exclusão de produtos no banco de dados, oferecendo uma solução simples e eficiente para gerenciar o catálogo de produtos.")
